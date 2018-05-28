3466664fecc562a84e6bcb44d8a832438c9c2238
Carl Bolduc <contact@carlbolduc.com>
2018-05-28 16:14:07 -0400

    Add search box

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { debounce } from 'lodash';
 import './index.css';
 import Result from './components/result.js';
 import Footer from './components/footer.js';
@@ -8,6 +9,7 @@ class SearchManager extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            query: '',
             results: []
         };
     }
@@ -23,14 +25,47 @@ class SearchManager extends React.Component {
             });
     }
 
+    getInfo = debounce(
+        () => {
+            fetch(`http://veganrealm.net:8080/recipes/${this.state.query}`)
+                .then(response => response.json())
+                .then(json => {
+                    this.setState({
+                        results: json
+                    });
+                });
+        },
+        300);
+
+    handleInputChange = () => {
+        this.setState({
+            query: this.search.value
+        }, () => {
+            if (this.state.query && this.state.query.length > 1) {
+                if (this.state.query.length % 2 === 0) {
+                    this.getInfo()
+                }
+            }
+        })
+    };
+
     render() {
         return (
-            <div id="results">
-                {this.state.results.map(result =>
-                    <Result key={result.id} result={result}/>
-                )}
-                <div>Yo</div>
-                <Footer recipesCount='22'/>
+            <div id="container">
+                <form>
+                    <input
+                        placeholder="Search for..."
+                        ref={input => this.search = input}
+                        onChange={this.handleInputChange}
+                    />
+                </form>
+                <div id="results">
+                    {this.state.results.map(result =>
+                        <Result key={result.id} result={result}/>
+                    )}
+                    <div>Yo</div>
+                    <Footer recipesCount='22'/>
+                </div>
             </div>
         );
     }
